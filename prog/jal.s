fe7cd96ff7a00a04c0fb2f83bdbca3268d8f3cf2
Luka Jankovic <lukjan1999@gmail.com>
2024-07-19 02:18:16 +0200

    changed to gcc to make call work, improved jal/r test

--- prog/jal.s
+++ prog/jal.s
@@ -1,3 +1,5 @@
+.global _start
+_start:
     addi a0, a0, 1
     addi a0, a0, 2
     nop
@@ -14,10 +16,24 @@ test:
     nop
     nop
     nop
+    call funcs
     addi a0, a0, 7
+    call func
 loop:
-    jal a1, loop
+    j loop
     addi a0, a0, 8
     addi a0, a0, 9
     addi a0, a0, 10
     addi a0, a0, 11
+func:
+    addi a0, a0, 10
+    addi a0, a0, 10
+    addi a0, a0, 10
+    addi a0, a0, 10
+    ret
+    addi a0, a0, 20
+    addi a0, a0, 20
+    addi a0, a0, 20
+    addi a0, a0, 20
+funcs:
+    ret
